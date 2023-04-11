a24fa2cc888775054dc356803a62acb5074c8d85
Spherz <spherzspro228@gmail.com>
2023-04-11 16:45:26 +0200

    add food table

--- app/src/main/java/com/example/fooddiary/util/DatabaseHelper.java
+++ app/src/main/java/com/example/fooddiary/util/DatabaseHelper.java
@@ -19,6 +19,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String GOAL = "goal";
     public static final String PHONE = "phone";
 
+    public static final String FOOD_TABLE = "FOOD";
+
+    public static final String FOOD_ID = "id";
+    public static final String FOOD_NAME = "food_name";
+    public static final String FOOD_CALORIES = "food_calories";
+    public static final String FOOD_FATS = "food_fats";
+    public static final String FOOD_CARBOHYDRATES = "food_carbohydrates";
+    public static final String FOOD_PROTEIN = "food_protein";
+
     static final String DB_NAME = "SECRET_IDEAL.DB";
 
     static final int DB_VERSION = 2;
@@ -28,6 +37,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + PHONE + " TEXT, " + PASSWORD + " TEXT NOT NULL, " + AGE + " TEXT, " + GENDER + " TEXT, "
             + ACTIVITY + " TEXT, " + WEIGHT + " TEXT, " + HEIGHT + " TEXT, " + GOAL + " TEXT);";
 
+    public static final String CREATE_FOOD_TABLE = "create table " + FOOD_TABLE + "(" + FOOD_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + FOOD_NAME + " TEXT, " + FOOD_CALORIES + " TEXT, "
+            + FOOD_FATS + " TEXT, " + FOOD_CARBOHYDRATES + " TEXT, " + FOOD_PROTEIN + " TEXT);";
+
 
     public DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -36,11 +49,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
+        db.execSQL(CREATE_FOOD_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + FOOD_TABLE);
         onCreate(db);
     }
 }
